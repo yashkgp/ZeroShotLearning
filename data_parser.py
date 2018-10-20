f50c57782b6c1d1736402681d1702e6d2a7a315a
Yash Jain <yashjainjain1704@gmail.com>
2018-10-20 16:16:50 +0530

    Adds file for evaluation

--- data_parser.py
+++ data_parser.py
@@ -3,12 +3,13 @@ import re
 import json
 from bs4 import BeautifulSoup
 import numpy as np
+from scipy import sparse
 
 class DataParser():
 
     def __init__(self):
         self.tags_split_pattern = re.compile('[><]')
-        self.ques_cleaning_pattern = re.compile("[\s\n\r\t.,:;\-_\'\"?!#&()]")
+        self.ques_cleaning_pattern = re.compile("[\s\n\r\t.,:;\-_\'\"?!#&()\/%\[\]\{\}\<\>\\$@\!\*\+\=]")
         pass
 
     def get_tags(self, tags_file, min_ct = 0, target_filename = None):
@@ -27,15 +28,18 @@ class DataParser():
             	json.dump(filtered_tags,f)
 
             del filtered_tags
+        else:
+            del root
+            del tree
+            return filtered_tags
         del root
         del tree
-        return filtered_tags
 
     def _split_tags(self,tags_string):
         t = tags_string[1:-1]
         return [x for x in self.tags_split_pattern.split(t) if x != '']
 
-    def get_posts_and_tags(self,posts_file, target_filename = None):
+    def get_posts_and_tags(self,posts_file, target_filename = None,max_ques = -1):
         tree = ET.parse(posts_file)
         root = tree.getroot()
         #posts = []
@@ -53,7 +57,15 @@ class DataParser():
                 json_list.append(temp)
             except:
                 pass
-
+        if max_ques != -1:
+            json_list_filtered = []
+            perm = np.random.permutation(len(json_list))[:max_ques]
+            for p in perm:
+                json_list_filtered.append(json_list[p])
+            del json_list
+            json_list = json_list_filtered
+            del json_list_filtered
+        print(len(json_list))
         if target_filename is not None:
         	with open(target_filename,'w') as f:
         		json.dump(json_list,f)
@@ -74,6 +86,7 @@ class DataParser():
                 if word not in counts:
                     counts[word] = 0
                 counts[word] += 1
+
         for key,value in counts.items():
             temp = {}
             if (value > min_count):
@@ -114,9 +127,45 @@ class DataParser():
                     temp[vocab_rev['<unk>']] += 1
             bow.append(temp)
 
-        bow = np.array(bow)
+        #bow = np.array(bow)
         if target_filename is not None:
             np.save(target_filename, bow)
             del bow
         else:
             return bow
+
+    def get_tags_one_hot(self,tags_file,posts_file,target_filename=None,to_sparse=False):
+        with open(tags_file) as f:
+            tags_list = json.load(f)
+
+        tags_rev_list = {}
+        i = 0
+        for tag in tags_list:
+            tags_rev_list[tag] = i
+            i += 1
+
+        one_hot = []
+        num_tags = len(tags_list)
+        del tags_list
+        with open(posts_file) as f:
+            posts_list = json.load(f)
+        for post in posts_list:
+            ques_tags = post['Tags']
+            temp = np.zeros(num_tags)
+            for tag in ques_tags:
+                try:
+                    temp[tags_rev_list[tag]] = 1
+                except:
+                    pass
+            one_hot.append(temp)
+        del posts_list
+        if to_sparse:
+            one_hot = sparse.csr_matrix(one_hot)
+        if target_filename is not None:
+            if to_sparse:
+                one_hot = sparse.csr_matrix(one_hot)
+                sparse.save_npz(target_filename,one_hot)
+            else:
+                np.save(target_filename,one_hot)
+        else:
+            return one_hot
